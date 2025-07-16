c1edb4d089f8558409fa1c6b69f8fc5c468d04cb
aadityasah <aaditya.sah365@gmail.com>
2025-07-16 18:13:00 -0400

    added LokiRobot code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/LokiRobot2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/LokiRobot2025.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode.common.robot;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 public class LokiRobot2025 {
+    HardwareMap mainMap;
+
+    public LiftPivot liftPivot;
+    public Arm arm;
+    public MecanumDrive drive;
+    private ElapsedTime robotTimer;
+
+    public LokiRobot2025(HardwareMap hMap){
+        init(hMap);
+    }
+
+    public void init(HardwareMap hMap){
+        mainMap = hMap;
+        liftPivot = new LiftPivot(hMap);
+        arm = new Arm(hMap);
+        drive = new MecanumDrive(hMap);
+        robotTimer = new ElapsedTime();
+    }
+
+    public void update(){
+        liftPivot.update();
+    }
 }
